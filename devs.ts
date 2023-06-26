dbe11b83a254c6c1a4f79fb77aa7f2c9190db7c4
Pablo Fernandes de Jesus <44382141+blito1998@users.noreply.github.com>
2023-06-25 21:37:59 -0300

    Add files via upload

---
+++ devs.ts
@@ -0,0 +1,113 @@
+export const devs = [
+  {
+    id: 1,
+    email: "assisjuniorwm@gmail.com",
+    password: "123456",
+    name: "Assis",
+    banner:
+      "https://images.unsplash.com/photo-1515704089429-fd06e6668458?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+    foto: "https://avatars.githubusercontent.com/u/62892419?v=4",
+    age: 36,
+    description:
+      "Aluno da 12ª edição do curso de Desenvolvimento Web Fullstack da Growdev",
+    stack: ["FullStack"],
+    hardSkills: [
+      "HTML",
+      "CSS",
+      "Javascript",
+      "Typescript",
+      "Bootstrap",
+      "React",
+      "Redux",
+      "NodeJs",
+      "Express",
+    ],
+    softSkills: [
+      'Trabalha bem em equipe', 
+      'Gosta de aprender', 
+      'Focado na tarefa'
+    ],
+  },
+  {
+    id: 2,
+    email: "vitor@mail.com",
+    password: "123456",
+    name: "Vitor",
+    banner:
+      "https://images.unsplash.com/photo-1515704089429-fd06e6668458?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+    foto: "https://th.bing.com/th/id/OIP.2cOe4ej-roywKGJXcvQQUQHaF7?pid=ImgDet&rs=1",
+    age: 22,
+    description:
+      "Aluno da 12ª edição do curso de Desenvolvimento Web Fullstack da Growdev",
+    stack: ["FullStack"],
+    hardSkills: [
+      "HTML",
+      "CSS",
+      "Javascript",
+      "Typescript",
+      "Bootstrap",
+      "React",
+      "Redux",
+      "NodeJs",
+      "Express",
+    ],
+    softSkills: [
+      "Trabalha bem em equipe",
+      "Gosta de aprender",
+      "Focado na tarefa"
+    ],
+  },
+  {
+    id: 3,
+    email: "davidlustosa3@gmail.com",
+    password: "123456",
+    name: "David",
+    banner:
+      "https://images.unsplash.com/photo-1515704089429-fd06e6668458?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+    foto: "https://avatars.githubusercontent.com/u/115047948?v=4",
+    age: 22,
+    description:
+      "Aluno da 12ª edição do curso de Desenvolvimento Web Fullstack da Growdev",
+    stack: ["FullStack"],
+    hardSkills: [
+      "HTML",
+      "CSS",
+      "Javascript",
+      "Typescript",
+      "Bootstrap",
+      "React",
+      "Redux",
+      "NodeJs",
+      "Express",
+    ],
+    softSkills: [],
+  },
+  {
+    id: 4,
+    email: "pablofernandes1997@hotmail.com",
+    password: "123456",
+    name: "Pablo Fernandes de Jesus",
+    banner:
+      "https://images.unsplash.com/photo-1515704089429-fd06e6668458?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+    foto: "https://avatars.githubusercontent.com/u/44382141?v=4",
+    age: 24,
+    description:
+      "Aluno da 12ª edição do curso de Desenvolvimento Web Fullstack da Growdev","Bacharel em Sistemas da Informação",
+    stack: ["FullStack"],
+    hardSkills: [
+      "HTML",
+      "CSS",
+      "Javascript",
+      "Typescript",
+      "Bootstrap",
+      "React",
+      "Redux",
+      "NodeJs",
+      "Express",
+      "C",
+      "Python"
+
+    ],
+    softSkills: [],
+  },
+];
