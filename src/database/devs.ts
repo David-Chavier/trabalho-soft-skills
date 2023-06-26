fbae24278319ca5ac81d8da4b5b4727bad870ae9
Pablo Fernandes de Jesus <44382141+blito1998@users.noreply.github.com>
2023-06-25 21:42:37 -0300

    Update devs.ts

--- src/database/devs.ts
+++ src/database/devs.ts
@@ -84,15 +84,15 @@ export const devs = [
   },
   {
     id: 4,
-    email: "pablo@mail.com",
+    email: "pablofernandes1997@hotmail.com",
     password: "123456",
-    name: "Pablo",
+    name: "Pablo Fernandes de Jesus",
     banner:
       "https://images.unsplash.com/photo-1515704089429-fd06e6668458?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
-    foto: "https://th.bing.com/th/id/OIP.0oY7nzQqpG1bEHQQobbBWAHaE7?pid=ImgDet&rs=1",
+    foto: "https://avatars.githubusercontent.com/u/44382141?v=4",
     age: 24,
     description:
-      "Aluno da 12ª edição do curso de Desenvolvimento Web Fullstack da Growdev",
+      "Aluno da 12ª edição do curso de Desenvolvimento Web Fullstack da Growdev","Bacharel em Sistemas da Informação",
     stack: ["FullStack"],
     hardSkills: [
       "HTML",
@@ -104,6 +104,8 @@ export const devs = [
       "Redux",
       "NodeJs",
       "Express",
+      "C",
+      "Python"
     ],
     softSkills: [],
   },
