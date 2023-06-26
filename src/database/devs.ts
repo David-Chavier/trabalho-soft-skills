e7d03544539537f24e97aebcce5449ca732033ac
Vitor-da-Silva-Dias <diasdasilvavitor@gmail.com>
2023-06-25 23:39:02 -0300

    update avatar and sofskills

--- src/database/devs.ts
+++ src/database/devs.ts
@@ -31,13 +31,13 @@ export const devs = [
   },
   {
     id: 2,
-    email: "vitor@mail.com",
+    email: "diasdasilvavitor@mail.com",
     password: "123456",
     name: "Vitor",
     banner:
       "https://images.unsplash.com/photo-1515704089429-fd06e6668458?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
-    foto: "https://th.bing.com/th/id/OIP.2cOe4ej-roywKGJXcvQQUQHaF7?pid=ImgDet&rs=1",
-    age: 22,
+    foto: "https://avatars.githubusercontent.com/u/113362918?u=fe1ebc87d2741dc2a447e7a51a69dbb9b47c7d1b&v=4",
+    age: 30,
     description: [
       "Aluno da 12ª edição do curso de Desenvolvimento Web Fullstack da Growdev",
     ],
@@ -79,7 +79,7 @@ export const devs = [
       "NodeJs",
       "Express",
     ],
-    softSkills: [],
+    softSkills: ["Tolerante, Bem humorado, Criativo"],
   },
   {
     id: 4,
@@ -108,6 +108,6 @@ export const devs = [
       "C",
       "Python",
     ],
-    softSkills: [],
+    softSkills: ["Autodidata, Trabalha bem em equipe, Solidário"],
   },
 ];
